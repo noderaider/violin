8a72ebdc1007d632f5ed577028b12f49a8562899
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-23 14:47:00 -0700

    added bunyan logging and made uncaughtException handling better

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,61 +1,82 @@
+import { createLogger } from 'bunyan'
 const should = require('chai').should()
 
 export default function violin(opts) {
-  function instrument({ tracing = false, logger = console, logLevel = 'info' } = {}) {
-    const log = (...args) => tracing ? logger[logLevel](...args) : () => {}
-    should.exist(opts.instrument, 'an instrument configuration option must be defined to use instrument()')
+  function instrument({ log = createLogger({ name: 'violin_instrument', streams: [ { path: './violin.log' } ] }) } = {}) {
+    should.exist(opts.instrument, 'violin: an instrument configuration option must be defined to use instrument()')
     let cancelMemoryPolling = () => {}
     const { memory } = opts.instrument
     if(memory) {
       const { startup, frequency, events } = memory
       const heapdump = require('heapdump')
-      if(startup) {
-        startup.should.be.a('boolean')
-        log('writing startup periodic heapdump.')
-        heapdump.writeSnapshot((heapErr, filename) => {
+
+      let isWriting = false
+      let snapshotCount = 0
+      function violinSnapshot(snapshotType) {
+        if(isWriting) {
+          log.warn(`violin: a memory snapshot is still being written, bypassing ${snapshotType} memory snapshot. Consider increasing the snapshot frequency interval.`)
+          return
+        }
+        snapshotCount++
+        isWriting = true
+        log.info(`violin: ${snapshotType} memory snapshot #${snapshotCount} about to be written.'`)
+        heapdump.writeSnapshot(function violinSnapshotComplete(heapErr, filename) {
           if(heapErr)
-            logger.error({ err: heapErr }, `error occurred writing startup heap at ${filename}.`)
+            log.error(heapErr, `violin: error occurred writing ${snapshotType} memory snapshot #${snapshotCount} at ${filename}.`)
           else
-            log(`startup periodic heapdump written to ${filename}.'`)
+            log.info(`violin: ${snapshotType} memory snapshot #${snapshotCount} written to ${filename}.'`)
+          isWriting = false
         })
       }
+
+      if(startup) {
+        startup.should.be.a('boolean')
+        log.info('violin: writing startup memory snapshot.')
+        violinSnapshot('startup')
+      }
       if(frequency) {
-        frequency.should.be.a('number')
-        let periodicCount = 0
+        frequency.should.be.a('number', 'violin: memory snapshot frequency must be a number')
         const frequencyMinutes = (frequency / (1000 * 60 * 60)).toLocaleString()
-        log(`starting periodic heapdump snapshots every ${frequencyMinutes} minutes.`)
-        let memoryIntervalID = setInterval(() => heapdump.writeSnapshot((heapErr, filename) => {
-          periodicCount++
-          if(heapErr)
-            logger.error({ err: heapErr }, `error occurred writing heap at ${filename}.`)
-          else
-            log(`periodic heapdump ${periodicCount} written to ${filename} (every ${frequencyMinutes} minutes).'`)
-        }), frequency)
+        log.info(`violin: starting periodic heapdump snapshots every ${frequencyMinutes} minutes.`)
+        let memoryIntervalID = setInterval(function violinFrequencySnapshot() { violinSnapshot('frequency') }, frequency)
         cancelMemoryPolling = () => clearInterval(memoryIntervalID)
       }
       if(events) {
-        Array.isArray(events).should.be.true
-        log({ events }, 'registering heap dump for events.')
+        events.should.be.instanceof(Array)
+        log.info({ events }, 'violin: registering heap dump for events.')
         events.forEach(x => {
-          should.exist(x, 'each event must exist')
-          x.should.be.a('string', 'each event must be a string eventName')
+          should.exist(x, 'violin: each event must exist')
+          x.should.be.a('string', 'violin: each event must be a string process eventName')
 
-          process.on(x, err => {
-            try {
-              heapdump.writeSnapshot((heapErr, filename) => {
-                if(heapErr) {
-                  log({ err: heapErr }, `error occurred writing heap at ${filename}`)
-                } else if(x === 'uncaughtException') {
-                  logger.fatal({ err }, `subscribed instrument memory process event 'uncaughtException' was triggered, snapshot written to ${filename}. exiting...`)
-                  process.exit(1)
-                } else {
-                  log(`heapdump written to ${filename} for subscribed memory process event '${x}'`)
-                }
+          if(x === 'uncaughtException') {
+            process.on('uncaughtException', function violinUncaughtExceptionSnapshot (err) {
+              // prevent infinite recursion
+              process.removeListener('uncaughtException', arguments.callee)
+
+              // log the exception
+              log.fatal(err, `violin: subscribed instrument memory process event 'uncaughtException' was triggered, snapshot written to ${filename}. exiting...`)
+              violinSnapshot('event_uncaughtException')
+
+              if (typeof(log.streams[0]) !== 'object') return
+
+              // throw the original exception once stream is closed
+              log.streams[0].stream.on('close', function violinFatalStreamClose (streamErr, stream) {
+                throw err
               })
-            } catch(err) {
-              console.error(err, 'HEAPDUMP_ERROR: An error occurred attempting to capture heap dump.')
-            }
-          })
+
+              // close stream, flush buffer to disk
+              log.streams[0].stream.end()
+            })
+
+          } else {
+            process.on(x, function violinEventSnapshot (err) {
+              try {
+                violinSnapshot(`event_${x}`)
+              } catch(err) {
+                log.error(err, `violin: an internal error occurred attempting to capture heap dump for event ${x}.`)
+              }
+            })
+          }
         })
       }
     }
@@ -63,3 +84,4 @@ export default function violin(opts) {
   }
   return { instrument }
 }
+
